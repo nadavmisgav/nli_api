90d5f86e179e6f55214b3645caf31803391c392b
ned3144 <nadav.misgav@gmail.com>
2022-04-23 20:32:40 +0300

    Bump version: 0.1.0 → 0.2.0

--- nli_api/__init__.py
+++ nli_api/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Nadav Misgav"""
 __email__ = 'nadav.misgav@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .api_ruleset import Where
 from .nli_api import get_nli_api
